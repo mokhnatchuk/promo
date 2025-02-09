5b7118a43a277a01a6d232015321bc2d70583290
Vasyl Mokhnatchuk <144223353+mokhnatchuk@users.noreply.github.com>
2025-02-09 10:40:26 +0200

    Setup main app and update settings

--- promo_project/promo_project/urls.py
+++ promo_project/promo_project/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from promo_project.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', include('main.urls')),
     path('products/', include('products.urls')),
     path('promotions/', include('promotions.urls')),
     path('stores/', include('stores.urls')),
