c8bc9aade0e68b25a74020e6e6ea767d93d90245
Vsevolod Parashchyn <sevaparashchyn@gmail.com>
2025-03-09 01:17:59 +0200

    Add models for products, stores, and promotions

--- promo_project/promotions/models.py
+++ promo_project/promotions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Promotion(models.Model):
+    name = models.CharField(max_length=255)
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField() 
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Promotion'
+        verbose_name_plural = 'Promotions'
--- promo_project/stores/models.py
+++ promo_project/stores/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=512) 
+    city = models.CharField(max_length=255)
+    working_hours = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Store'
+        verbose_name_plural = 'Stores'
